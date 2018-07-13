b592a22c94a3cbc66ec72d931b5c6be03673b3fd
timgrossmann <t.grossmann1994@web.de>
2018-07-13 23:58:42 +0200

    cleanup, change structure

--- index.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-const app = express()
-
-// static file serve
-// not found in static files, so default to index.html
-app.use(express.static(__dirname))
-app.use((req, res) => res.sendFile(`${__dirname}/index.html`))
-
-app.listen(3000, () => console.log('State of veganism is running on port 3000'))
